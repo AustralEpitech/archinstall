fa579eba0ba2c0fa80535cc53e0666a788232f4f
ange <ange@yw5n.com>
2025-06-21 06:41:23 +0000

    fix: dotfiles url

--- modules/10-chroot/user/10-dotfiles.sh
+++ modules/10-chroot/user/10-dotfiles.sh
@@ -2,10 +2,10 @@
 #shellcheck disable=SC2154
 
 config=(git --git-dir "$HOME/.dotfiles" --work-tree "$HOME")
-repo='https://git.maby.dev/ange/.dotfiles.git'
+repo='https://git.gmoker.com/ange/dotfiles.git'
 
 git clone --bare "$repo" "$HOME/.dotfiles"
 "${config[@]}" checkout -f
 "${config[@]}" submodule update --init --recursive --remote
 "${config[@]}" config status.showUntrackedFiles no
-"${config[@]}" remote set-url origin git@git.maby.dev:ange/.dotfiles.git
+"${config[@]}" remote set-url origin git@git.gmoker.com:ange/dotfiles.git
