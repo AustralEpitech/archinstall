f46871a28a185952756e921beafb531b845e242f
ange <ange@yw5n.com>
2025-10-03 10:31:25 +0000

    fix: sbctl not in archiso

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+pacman -Sy sbctl --noconfirm
+
 if ! sbctl status | grep -q '^Setup Mode:.*Enabled$'; then
     printf '%s\n' \
         'If you want Secure Boot support, you need to put your system in Setup Mode' \
         'See https://wiki.archlinux.org/title/Unified_Extensible_Firmware_Interface/Secure_Boot'
     read -rp "Reboot into the firmware setup interface? [Y/n] " ANS
-    if ! [[ "${ANS,}" =~ ^$|^y ]]; then
+    if [[ "${ANS,}" =~ ^$|^y ]]; then
         systemctl reboot --firmware-setup
     fi
 fi
